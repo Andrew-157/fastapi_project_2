b4cb6820d3f8d4d4d841c4424e2f1d9865d1fc0d
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-28 10:51:57 +0200

    Created User model for database based on SQLModel

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from sqlmodel import SQLModel, Field
+from .schemas import UserBase
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    hashed_password: str
---
+++ app/schemas.py
@@ -0,0 +1,16 @@
+import re
+from sqlmodel import SQLModel, Field
+from pydantic import validator
+
+
+class UserBase(SQLModel):
+    username: str = Field(unique=True,
+                          min_length=5, max_length=255, index=True)
+
+    email: str = Field(unique=True, max_length=255, index=True)
+
+    def email_valid(cls, value):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        if not re.fullmatch(regex, value):
+            raise ValueError("Not valid email")
+        return value
