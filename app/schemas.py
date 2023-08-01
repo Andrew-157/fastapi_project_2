2541ac489de6a45f405a55f4d67989d590cc2af2
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-01 17:26:33 +0200

    Connected to alembic and migrated

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,18 @@
+import re
 from sqlmodel import SQLModel, Field
-from pydantic import validator, EmailStr
+from pydantic import validator
 
 
 class UserBase(SQLModel):
     username: str = Field(max_length=255, min_length=5,
                           unique=True, index=True)
 
-    email: EmailStr = Field(max_length=255, unique=True,
-                            index=True)
+    email: str = Field(max_length=255, unique=True,
+                       index=True)
+
+    @validator("email")
+    def email_valid(cls, value):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        if not re.fullmatch(regex, value):
+            raise ValueError("Not valid email address")
+        return value
