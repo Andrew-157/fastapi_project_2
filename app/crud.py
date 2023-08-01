fe0e96c3384f5ab247f371a6a47328affd98cf0f
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-01 17:35:21 +0200

    Auth file
    Different tools for dealing with authorization-related path operations

--- app/crud.py
+++ app/crud.py
@@ -2,9 +2,9 @@ from sqlmodel import Session, select
 from .models import User
 
 
-def get_user_with_username(session: Session, username: str):
+def get_user_with_username(session: Session, username: str) -> User:
     return session.exec(select(User).where(User.username == username)).first()
 
 
-def get_user_with_email(session: Session, email: str):
+def get_user_with_email(session: Session, email: str) -> User:
     return session.exec(select(User).where(User.email == email)).first()
