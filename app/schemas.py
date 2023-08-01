dfeba1d94ea2e80fc3abbe774c6c93fdc30a2592
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-01 18:08:06 +0200

    users path operations

--- app/schemas.py
+++ app/schemas.py
@@ -16,3 +16,24 @@ class UserBase(SQLModel):
         if not re.fullmatch(regex, value):
             raise ValueError("Not valid email address")
         return value
+
+
+class UserCreate(UserBase):
+    password: str = Field(min_length=8)
+
+
+class UserRead(UserBase):
+    id: int
+
+
+class UserUpdate(SQLModel):
+    username: str = Field(max_length=255, min_length=5,
+                          default=None)
+    email: str = Field(max_length=255, default=None)
+
+    @validator("email")
+    def email_valid(cls, value):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        if not re.fullmatch(regex, value):
+            raise ValueError("Not valid email address")
+        return value
