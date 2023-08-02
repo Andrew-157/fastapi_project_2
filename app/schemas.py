4dfba3d1ffa51162e70a63b8bd87165f2c9f1f8b
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-02 08:50:32 +0200

    Additionla schemas for reading amd creating recommendations

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 from sqlmodel import SQLModel, Field
 from pydantic import validator
 
@@ -24,14 +25,38 @@ class RecommendationBase(SQLModel):
     opinion: str
 
 
+class RecommendationCreate(RecommendationBase):
+    fiction_type: str = Field(min_length=4,
+                              max_length=255,
+                              unique=True)
+    tags: list[str] = Field(min_items=1)
+
+
 class FictionTypeBase(SQLModel):
     name: str = Field(min_length=4,
                       max_length=255,
                       unique=True)
 
 
+class FictionTypeRead(FictionTypeBase):
+    id: int
+
+
 class TagBase(SQLModel):
     name: str
+
+
+class TagRead(TagBase):
+    id: int
+
+
+class RecommendationRead(RecommendationBase):
+    id: int
+    fiction_type: FictionTypeRead
+    user_id: int
+    published: datetime
+    updated: datetime
+    tags: list[TagRead]
 
 
 class UserCreate(UserBase):
